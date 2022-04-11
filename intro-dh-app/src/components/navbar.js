182318fb1bf0d837744aa7cee42ba433da2bcdde
Kyra Chan <ktc22@duke.edu>
2022-04-11 17:25:34 -0400

    create articles page

--- intro-dh-app/src/components/navbar.js
+++ intro-dh-app/src/components/navbar.js
@@ -6,12 +6,12 @@ function Menu() {
     return (
         <Navbar bg="dark" sticky="top">
             <Container>
-                <Navbar.Brand href="#" className="white me-4">Music Trends</Navbar.Brand>
+                <Navbar.Brand href="/" className="white me-4">Music Trends</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ms-4 me-auto justify-content-evenly">
-                    <Nav.Link href="#" className="white me-2">Data Visualizations</Nav.Link>
-                    <Nav.Link href="#articles" className="white">Articles</Nav.Link>
+                    <Nav.Link href="/" className="white me-2">Data Visualizations</Nav.Link>
+                    <Nav.Link href="articles" className="white">Articles</Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
